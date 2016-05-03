1db9e2b261c331c43483917c5b7592659285894e
Jesse Wang <jesse.wang19@gmail.com>
2016-05-02 22:25:59 -0400

    Added files via upload

---
+++ cs-exercises.js
@@ -0,0 +1,83 @@
+/***********************/
+/*  Conditional Statement Exercises */
+/***********************/
+
+/*
+  Create a function named 'example' with one parameter.
+  Name the parameter anything you want
+  Return "true" if the parameter is true, otherwise do nothing
+  ie:
+    exampleTwo(true) -> "true"
+*/
+function exampleTwo(flag) {
+  if (flag) {
+    return "true";
+  }
+}
+
+/*
+  Create a function named 'checkBirthday' with one parameter.
+  Name the parameter anything you want
+  Return "It's you're birthday!" if the parameter is true, otherwise do nothing
+  ie:
+    checkBirthday(true) -> "It's you're birthday!"
+*/
+
+
+/*
+  Create a function named 'checkHoliday' with one parameter.
+  Name the parameter anything you want
+  Return "It's time to go to school!" if the parameter is false, otherwise do nothing
+  ie:
+    checkHoliday(true) -> "It's time to go to school!"
+*/
+
+
+/*
+  Create a function named 'light' with one parameter.
+  Name the parameter anything you want
+  Return "Turn light off" if the parameter is true, otherwise return "Turn light on"
+  ie:
+    light(true) -> "Turn light off"
+    light(false) -> "Turn light on"
+*/
+
+
+/*
+  Create a function named 'engine' with two parameters.
+  Name the parameters anything you want
+  Return "Rev up" if the parameter is true, otherwise return "Shutdown"
+  ie:
+    engine(true, true) -> "Rev up"
+    engine(true, false) -> "Shutdown"
+    engine(false, true) -> "Shutdown"
+    engine(false, false) -> "Shutdown"
+*/
+
+
+/*
+  Create a function named 'gradeScore' with one parameter.
+  Name the parameter anything you want
+  Return "A" if the parameter is greater than 90, "B" if the parameter is greater than 80, otherwise return false
+  ie:
+    gradeScore(91) -> "A"
+    gradeScore(90) -> "B"
+    gradeScore(81) -> "B"
+    gradeScore(80) -> "C"
+*/
+
+
+/*
+  Create a function named 'imDone' with three parameters.
+  Name the parameters anything you want
+  Return "yes" if at least two parameters are true, otherwise return "no"
+  ie:
+    imDone(true, true, true) -> "yes"
+    imDone(true, true, false) -> "yes"
+    imDone(true, false, true) -> "yes"
+    imDone(false, true, true) -> "yes"
+    imDone(true, false, false) -> "no"
+    imDone(false, true, false) -> "no"
+    imDone(false, false, true) -> "no"
+    imDone(false, false, false) -> "no"
+*/
---
+++ function-exercises.js
@@ -0,0 +1,64 @@
+/***********************/
+/*  Function Exercises */
+/***********************/
+
+/*
+  Create a function named 'example' with no parameters.
+  Leave the body empty
+*/
+function example() {
+
+}
+
+
+/*
+  Create a function named 'ready' with no parameters.
+  Leave the body empty
+*/
+
+
+/*
+  Create a function named 'set' with one parameter.
+  Name the parameter anything you want
+  Leave the body empty
+*/
+
+
+/*
+  Create a function named 'go' with two parameters.
+  Name the parameters anything you want
+  Leave the body empty
+*/
+
+
+/*
+  Create a function named 'ditto' with one parameter.
+  Name the parameter anything you want
+  Returns the argument passed into the function
+  ie:
+    ditto(1) -> 1
+    ditto(-2) -> -2
+    ditto(3) -> 3
+*/
+
+
+/*
+  Create a function named 'negative' with one parameter.
+  Name the parameter anything you want
+  Returns the negative of the argument passed into the function
+  ie:
+    negative(10) -> -10
+    negative(20) -> -20
+    negative(30) -> -30
+*/
+
+
+/*
+  Create a function named 'reverse' with two parameters.
+  Name the two parameters anything you want
+  Returns a String composed of the two parameters with the second argument coming first
+  ie:
+    reverse("ball", "foot") -> "football"
+    reverse("bomb", "the") -> "thebomb"
+    reverse("der" "red") -> "redder"
+*/
